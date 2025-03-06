b9031c39d5a54ed4b6c20e7f81ac8d1e502024e0
Kjetil <haugkjet@gmail.com>
2025-03-06 17:16:26 +0100

    Added skyshader, can be optimized

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ import { addGltf } from './addGltf.js';
 
 import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment.js';
 
+import { ShaderMaterial, Vector2, Vector3 } from "three";
+
 // Sizes
 const sizes = {
     width: window.innerWidth,
@@ -24,13 +26,13 @@ const sizes = {
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
+THREE.ColorManagement.enabled = true;
+renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.toneMappingExposure = 1.0; 
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-THREE.ColorManagement.enabled = true;
-renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
 document.body.appendChild(renderer.domElement);
 
 window.addEventListener('resize', () =>
@@ -53,14 +55,14 @@ composer.addPass(renderPass);
 
 const bloomparams = {
     enableBloom: true,
-    bloomStrength: 0.3,
+    bloomStrength: 0.162,
     bloomRadius: 0.4,
     bloomThreshold: 0.496
   };
 
 const bloomPass = new UnrealBloomPass(
     new THREE.Vector2(window.innerWidth, window.innerHeight),
-    0.3,
+    0.162,
     0.4,
     0.496
   );
@@ -90,6 +92,63 @@ gui.add( document, 'title' );
     roomenv: true,
     roombackground: false
   };
+
+  const SKY_COLOR = 0x6ac5fe ;
+  const GROUND_COLOR = 0xdaf0ff;
+  const SKY_SIZE = 100;
+  
+  const vertexShader = `
+        varying vec3 vWorldPosition;
+              void main() {
+                  vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
+                  vWorldPosition = worldPosition.xyz;
+                  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+              }`;
+  const fragmentShader = `
+        uniform vec3 topColor;
+              uniform vec3 bottomColor;
+              varying vec3 vWorldPosition;
+              void main() {
+                  float h = normalize( vWorldPosition).y;
+                  gl_FragColor = vec4( mix( bottomColor, topColor, max( h, 0.0 ) ), 1.0 );
+              }`;
+  const uniforms = {
+    topColor: { value: new THREE.Color(SKY_COLOR) },
+    bottomColor: { value: new THREE.Color(GROUND_COLOR) },
+  };
+  const skyGeo = new THREE.SphereGeometry(SKY_SIZE, 32, 15);
+  const skyMat = new ShaderMaterial({
+    uniforms,
+    vertexShader,
+    fragmentShader,
+    side: THREE.BackSide,
+  });
+  const sky = new THREE.Mesh(skyGeo, skyMat);
+  scene.add(sky);
+
+  const skyRenderTarget = new THREE.WebGLCubeRenderTarget(1024);
+  const skyCamera = new THREE.CubeCamera(0.1, 1000, skyRenderTarget);
+  
+  const skyShader = new THREE.ShaderMaterial({
+    uniforms: {
+      topColor: { value: new THREE.Color(SKY_COLOR) },
+      bottomColor: { value: new THREE.Color(GROUND_COLOR) }
+    },
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader,
+    side: THREE.BackSide
+  });
+  
+  const skyMesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), skyShader);
+
+  function updateSky() {
+    skyMesh.visible = true;
+    skyCamera.update(renderer, scene);
+    skyMesh.visible = false;
+  }
+
+  scene.environment = skyRenderTarget.texture;
+
 
 
 
@@ -142,7 +201,7 @@ camera.position.x = -2;
 
 const geometryPlane = new THREE.PlaneGeometry(20, 20)
 const plane = new THREE.Mesh(geometryPlane, PixPalMaterial);
-adjustUVsToSinglePixel(geometryPlane,  49,45); // No colorpicker 
+adjustUVsToSinglePixel(geometryPlane,  49,35); // No colorpicker 
 scene.add(plane);
 plane.rotation.x = -Math.PI/2
 plane.position.y = 0
@@ -151,6 +210,9 @@ plane.position.y = 0
 
 addCubes(scene); // Use pixpal directly in threejs
 addGltf (scene, 0,0,-5,'./assets/madeinblender.glb')
+
+//addGltf (scene, 0,0,0,'./assets/Imphenzia-PixPal.glb')
+
 
 const clock = new THREE.Clock();
 
